b2d925bb0240115e66564f69528a67ca1bea6a4a
Youchul Song <youchul8989@naver.com>
2024-05-18 03:13:26 +0900

    초기 커밋.

---
+++ Project_busan2/20242390.c
@@ -0,0 +1,159 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+#include <time.h>
+#include <Windows.h>
+#define LEN_MIN 15 //기차길이
+#define LEN_MAX 50
+#define PROB_MIN 10 //확률
+#define PROB_MAX 90
+int main(void) {
+    srand((unsigned int)time(NULL));
+    //인트로 
+    printf("======================================\n");
+    printf("               부산헹                 \n");
+    printf("======================================\n");
+    Sleep(2000);
+    //열차의 길이와 확률 입력받기
+    int train_len; //기차 길이
+    int percent; //확률	
+    printf("train length(%d %d)>> ", LEN_MIN, LEN_MAX);
+    scanf_s("%d", &train_len);
+    printf("percentile probability 'p'(%d %d)>> ", PROB_MIN, PROB_MAX);
+    scanf_s("%d", &percent);
+    printf("\n");
+    if (train_len < 15 || train_len > 50 || percent < 10 || percent > 90) {
+        printf("잘못된 입력으로 프로그램을 종료합니다.\n");
+        return 0;
+    }
+    // 'C,Z'의 초기 위치
+    int cPosition = train_len - 6;
+    int zPosition = train_len - 3;
+    // 열차 상단 및 하단 경계 출력
+    for (int i = 0; i < train_len; i++) {
+        printf("#");
+    }
+    printf("\n#");
+    // 초기 열차 내부
+    for (int i = 1; i < train_len - 1; i++) { //
+        if (i == cPosition) {
+            printf("C");
+        }
+        else if (i == train_len - 3) {
+            printf("Z");
+        }
+        else if (i == train_len - 2) {
+            printf("M");
+            i += 1;
+        }
+        else {
+            printf(" ");
+        }
+    }
+    printf("#\n");
+    // 열차 상단 및 하단 경계 출력
+    for (int i = 0; i < train_len; i++) {
+        printf("#");
+    }
+    printf("\n");
+    //bool함수
+    bool cMoved = false;
+    bool zMoved = false;
+    bool canMoved = false;
+    //변수
+    int zombieCount = 0;
+    // 반복문 시작
+    while (1) {
+        // 4초 마다 
+        Sleep(4000);
+        // 반복마다 이동 여부 초기화
+        cMoved = false;
+        zMoved = false;
+        canMoved = false;
+        int randomNum = rand() % 100; // 0에서 99까지의 난수 생성
+
+        // 열차 상단 및 하단 경계 출력
+        printf("\n\n");
+        for (int i = 0; i < train_len; i++) {
+            printf("#");
+        }
+        printf("\n#");
+        // 입력 받은 확률로 cPosition-- or zPosition--
+        zombieCount += 1; // 게속 1식 올라가면서 zombieCount가 홀수일 때만 Z가 이동(zPosition--)
+        if (zombieCount % 2 == 1) {
+            if (randomNum < percent) {
+                zPosition--;
+                zMoved = true;
+            }
+            else {
+                zMoved = false;
+            }
+        }
+        else { //zombieCount가 짝수이면 Z는 움직이지 않고 cannot move
+            zMoved = false;
+            canMoved = true;
+        }
+        if (randomNum < 100 - percent) { //100-p 확률 //randomNum >= percent
+            cPosition--;
+            cMoved = true;
+        }
+        else {
+            cMoved = false;
+        }
+        // 열차 내부
+        for (int i = 1; i < train_len - 1; i++) { //열차길이 - 1 만큼 반복
+            if (i == cPosition) { //초기 cPosition은 train_len - 6, cPosition이 감소하면 그 위치에 문자출력 
+                printf("C");
+            }
+            else if (i == zPosition) {
+                printf("Z");
+            }
+            else if (i == train_len - 2) { //열차 끝에서 2번째에 M출력
+                printf("M");
+                i += 1;
+            }
+            else { //위 조건 해당안되면 공백 출력
+                printf(" ");
+            }
+        }
+        printf("#\n");
+
+        // 열차 하단 경계 출력
+        for (int i = 0; i < train_len; i++) {
+            printf("#");
+        }
+        printf("\n");
+        //시민, 좀비 상태 출력
+        printf("\n\n");
+        if (cMoved) {
+            printf("citizen: %d -> %d\n", cPosition + 1, cPosition);
+        }
+        else {
+            printf("citizen: stay %d\n", cPosition);
+        }
+        if (zMoved) {
+            printf("zombie: %d -> %d\n", zPosition + 1, zPosition);
+        }
+        else if (canMoved) {
+            printf("zombie: stay %d (cannot move)\n", zPosition);
+        }
+        else {
+            printf("zombie: stay %d\n", zPosition);
+        }
+        // C가 끝에 도착하면 탈출 성공 출력 후 프로그램 종료
+        if (cPosition == 1) { // 끝이 1
+            printf("\n");
+            printf("SUCCESS!\n");
+            printf("citizen(s) escaped to the next train\n");
+            //return 0;
+            break;
+        }
+        // Z가 C바로 옆에 도착하면 구출 실패 출력 후 프로그램 종료
+        if (cPosition == zPosition - 1) {
+            printf("\n");
+            printf("GAME OVER\n");
+            printf("citizen(s) has(have) been attacked by a zombie\n");
+            break;
+        }
+    }
+}
